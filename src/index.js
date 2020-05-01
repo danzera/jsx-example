788d1ac143b3ff6d9495d734a007abdd5048ed8c
Dan Zera <dczera@gmail.com>
2020-04-30 20:27:43 -0700

    add index.js file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a React component
+const App = () => {
+	return <div>Hey, there!</div>;
+}
+
+// Use the React component to render content
+ReactDOM.render(<App />, document.querySelector('#root'));
