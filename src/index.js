c629cf823d9945b6f6ea29047b82450d7b4b4845
Dan Zera <dczera@gmail.com>
2020-05-01 12:55:10 -0700

    update index.js to include examples for referencing JS and inline styling within JSX

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import ReactDOM from 'react-dom';
 
 // Create a React component
 const App = () => {
-	return <div>Hey, there!</div>;
+	// variables and functions can be referenced within JSX using single curly braces { }
+	// see return statement below
+	const buttonText = 'Click Me!';
+	function labelText() { return 'Enter name:' };
+
+	return (
+		<div>
+  		<label className="label" htmlFor="name">{ labelText() }</label>
+  		<input id="name" type="text" />
+  		<button style={{ backgroundColor: 'blue', color: 'white'}}>
+				{buttonText}
+			</button>
+		</div>
+	);
 }
 
 // Use the React component to render content
